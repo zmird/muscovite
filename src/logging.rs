bc5af8b5b29152c602f79ce150ada98a8cea8c7d
zmird <z@mird.com>
2020-05-13 23:50:58 +0200

    Set file logging to debug.

--- src/logging.rs
+++ src/logging.rs
@@ -3,6 +3,7 @@ use log4rs::append::console::ConsoleAppender;
 use log4rs::append::file::FileAppender;
 use log4rs::encode::pattern::PatternEncoder;
 use log4rs::config::{Appender, Config, Logger, Root};
+use log4rs::filter::threshold::ThresholdFilter;
 
 
 pub fn config_logs(filename: String) {
@@ -16,13 +17,14 @@ pub fn config_logs(filename: String) {
         .unwrap();
 
     let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .appender(Appender::builder()
+            .filter(Box::new(ThresholdFilter::new(LevelFilter::Info)))
+            .build("stdout", Box::new(stdout)))
         .appender(Appender::builder().build("logfile", Box::new(logfile)))
-        .logger(Logger::builder().build("logfile", LevelFilter::Info))
         .build(Root::builder()
             .appender("stdout")
             .appender("logfile")
-            .build(LevelFilter::Info))
+            .build(LevelFilter::Debug))
         .unwrap();
 
     log4rs::init_config(config).unwrap();
