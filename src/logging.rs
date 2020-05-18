60ba6c833eadfcb67b587acc13a7501402adcd88
zmird <z@mird.com>
2020-05-18 19:55:59 +0200

    Removed unused import.

--- src/logging.rs
+++ src/logging.rs
@@ -2,7 +2,7 @@ use log::LevelFilter;
 use log4rs::append::console::ConsoleAppender;
 use log4rs::append::file::FileAppender;
 use log4rs::encode::pattern::PatternEncoder;
-use log4rs::config::{Appender, Config, Logger, Root};
+use log4rs::config::{Appender, Config, Root};
 use log4rs::filter::threshold::ThresholdFilter;
 
 
